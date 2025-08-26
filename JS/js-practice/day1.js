e8f11c323a5dd46ad8859b2b7c581c84343453d7
Anindita531 <aninditaghosh3551@gmail.com>
2025-08-26 08:41:26 +0530

    Add files via upload

---
+++ JS/js-practice/day1.js
@@ -0,0 +1,16 @@
+console.log("Hello, JavaScript!");
+
+var name = "Anindita";
+console.log(name);  // Output: Anindita
+let age = 20;
+console.log(age);  // 20
+age = 21;
+console.log(age);  // 21
+
+
+const country = "India";
+console.log(country);  // India
+// country = "USA"; ❌ Error
+const day = 1;
+console.log(day);  //1
+//day = 2;❌ Error
