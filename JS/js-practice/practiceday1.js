3e0fee5a60280e39e7f34aa067b516442ba78f3c
Anindita531 <aninditaghosh3551@gmail.com>
2025-08-26 09:21:42 +0530

    Add files via upload

---
+++ JS/js-practice/practiceday1.js
@@ -0,0 +1,16 @@
+
+const name=`Anindita `;  
+let age=21;  
+let friends=["Anu","Me","Myself","I"];  
+details={name:"Anindita",branch:"IT",Roll:21};  
+console.log(`My name is ${name}`);  
+console.log("age:"+age);  
+console.log(friends);  
+console. log(details);  
+//task2
+let x = 5;
+let y = "5";
+
+console.log(x == y);// True
+console.log(x === y);//   False
+console.log(typeof y);//String  
